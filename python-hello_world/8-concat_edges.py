b6ba3c95a162f527424b8c9461fedf5ae7d7b5fd
haru-094 <a.nasir@alustudent.com>
2025-10-15 06:47:52 -0700

    fix code 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = f"{str[38:67]}{str[6]}{str[94:98]}{str[6]}{str[0:6]}"
+str = str[39:67] + str[0:6]
 print(str)
