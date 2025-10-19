fb157432cfd7e061f69e88ca6adc3432cb325c28
haru-094 <anas.k7aleed@gmail.com>
2025-10-19 18:39:10 +0200

    fix task

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,11 +2,9 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
-        while True:
-            if count == x:
-                break
-        print(my_list[count], end="")
-        count += 1
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
     except IndexError:
         pass
     print()
