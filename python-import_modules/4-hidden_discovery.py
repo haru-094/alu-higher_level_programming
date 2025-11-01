f6f5fa0064a617bc00ee63da2e8e7bb52a139123
haru-094 <anas.k7aleed@gmail.com>
2025-11-01 07:57:28 +0200

    fix task

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     sp = importlib.util.spec_from_file_location("hidden_4", mod_path)
     hidden_4 = importlib.util.module_from_spec(sp)
     sys.modules["hidden_4"] = hidden_4
-    spec.loader.exec_module(hidden_4)
-    names = [name for name in dir(hidden_4) if not name.startwith("__")]
+    sp.loader.exec_module(hidden_4)
+    names = [name for name in dir(hidden_4) if not name.startswith("__")]
     for name in sorted(names):
         print(name)
