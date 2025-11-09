bc27bd63bf91ffe91a86100a68b23ba9445c2a18
haru-094 <anas.k7aleed@gmail.com>
2025-11-09 08:25:23 +0200

    fix task

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,19 +9,18 @@ class Square:
     square with vaildation for size
     """
 
-    def __init__(self,size=0):
+    def __init__(self, size=0):
         """
         init the instance
 
         Args:
             size(int, optional): create new square size
-        
         Rasie:
             TypeError: if size not integer
             valueError: if the size is less 0
         """
         if not isinstance(size, int):
-            raise TypeError("size is not integer")
+            raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("size must be greater than 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
