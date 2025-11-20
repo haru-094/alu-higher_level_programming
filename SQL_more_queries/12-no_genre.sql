b56ec9b8b9ea13d50024202eecc1875bdd1da466
haru-094 <anas.k7aleed@gmail.com>
2025-11-20 09:30:52 +0200

    sql more queries

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,4 @@
+-- getting record
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+    LEFT JOIN tv_show_genres
---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,9 @@
+-- getting record
+SELECT cities.id, cities.name, (
+        SELECT name
+        FROM states
+        WHERE
+            states.id = cities.state_id
+    ) AS state_name
+FROM cities
+ORDER BY cities.id
